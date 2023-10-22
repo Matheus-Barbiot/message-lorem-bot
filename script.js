0b84eeac50026b19046c8102cabe150b5d2d9a56
Matheus Barbiot <matheusbarbiot01@gmail.com>
2023-10-22 12:07:32 -0300

    Bot enviar mensagens (lorem)

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+function multiplicarString(string, num) {
+    let res = ''
+    for (let i=0; i < num; i++){
+        res += string
+    }
+    return res
+}
+
 function enviar() {
     let mensagem = document.getElementById('msg').value
     let espacoMensagem = document.getElementById('mensagens')
     
     espacoMensagem.innerHTML += `<div><p>${mensagem}</p></div>`
+
+    setTimeout(botenviar, 3000)
+}
+
+function botenviar() {
+    const num = Math.floor(Math.random() * 3) + 1
+    let text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ut eius illo ratione nihil!'
+    let espacoMensagem = document.getElementById('mensagens')
+
+    espacoMensagem.innerHTML += `<div><p>${multiplicarString(text, num)}</p></div>`
 }
